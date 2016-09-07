e76b9a3074a2d4c05158ea84bc97aac9e911c580
Aaron <aaron.devleague@gmail.com>
2016-09-06 17:01:51 -1000

    2nd commit

--- basics.js
+++ basics.js
@@ -1,10 +1,13 @@
 /* Create a `myName` variable and assign it a String value */
-var test = a
+var myName = '';
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
-
+var person = {
+  name: myName,
+  age: 21,
+};
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
